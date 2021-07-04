d0da560983ccb3cf6c3b516fb7207f2fbe12d177
João Marcelo <joaooujdqsa@outlook.com>
2021-07-04 15:17:59 -0300

    algoritmo analitico

---
+++ AlgAnalitc/src/alganalitic/Desenha.java
@@ -0,0 +1,47 @@
+package alganalitic;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class Desenha extends JFrame{
+
+		public Desenha() {
+			
+			
+			
+			this.setTitle("Linha Reta - Analítico");
+			this.setSize(300,300);
+			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+			this.setLocationRelativeTo(null);
+			this.setVisible(true);
+		}
+		
+		@Override
+		public void paint(Graphics g) {
+			g.setColor(Color.red);
+			algAnalitic(g, 10, 50, 60, 100);
+			algAnalitic(g, 10, 50, 60, 50);
+			algAnalitic(g, 20, 50, 20, 100);
+		}
+		
+		public void algAnalitic(Graphics g, int xi, int yi, int xf, int yf) {
+			float m, b, dy, dx;
+			dy = yf - yi;
+			dx = xf - xi;
+			
+			m = (float) dy/dx;
+			
+			b= (float) (yi - m*xi);
+			
+			for(int x = xi; x <= xf; x++ ) {
+				int y = (int)(m*x+b);
+				putPixel(g,x,y);
+			}
+		}
+		
+		public void putPixel(Graphics g, int x, int y) {
+			g.drawLine(x, y, x , y);
+		}
+}
---
+++ AlgAnalitc/src/alganalitic/Main.java
@@ -0,0 +1,11 @@
+package alganalitic;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		new Desenha();
+
+	}
+
+}
