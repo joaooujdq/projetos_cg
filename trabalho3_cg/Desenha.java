eeb45a380ce5b4ebde70b9984658dd7fd9a52190
João Marcelo <49964202+joaooujdq@users.noreply.github.com>
2021-08-11 19:37:00 -0300

    corrigindo os ultimos erros

--- trabalho3_cg/Desenha.java
+++ trabalho3_cg/Desenha.java
@@ -17,15 +17,17 @@ public class Desenha extends JComponent {
 	public Desenha() {
 
 		// lados = new Lados[4];
-		vx = new Vx[312];
-		for (int i = 0; i < 312; i++)
-			vx[i] = new Vx();
+//		vx = new Vx[312];
+//		for (int i = 0; i < 312; i++)
+//			vx[i] = new Vx();
 
 	}
 
 	@Override
 	public void paint(Graphics g) {
 		int pontoSorteado;
+		int somaVx = 100;
+		int distPontos = 0;
 		Random gerador = new Random();
 		int numlados = gerador.nextInt(6) + 3;
 		System.out.println(numlados);
@@ -37,75 +39,87 @@ public class Desenha extends JComponent {
 			System.out.println(pontoSorteado);
 			pontos[i] = pontoSorteado;
 		}
+		
+		
 
 		g.setColor(Color.red);
-
-//        algBres(g,px1,py1,px2,py2); // i=0 
+//      algBres(g,px1,py1,px2,py2); // i=0 
 //        algBres(g,px2,py2,px3,py3); //i=1
 //        algBres(g,px3,py3,px4,py4);//i=2
 //        algBres(g,px4,py4,px1,py1);//i=3
 
 		for (int i = 0; i < numlados; i++) {
+		
 			if (i == numlados - 1) {
-				algBres(g, pontos[i * 2], pontos[i * 2] + 1, pontos[0], pontos[1]);
+				algBres(g, pontos[i * 2], pontos[(i * 2) + 1] , pontos[0], pontos[1]);
+				distPontos = (pontos[(i * 2) + 1] - pontos[1]);
+				distPontos = Math.abs(distPontos);
+				somaVx = somaVx + distPontos;
 			} else {
-				algBres(g, pontos[i * 2], pontos[i * 2] + 1, pontos[i * 2] + 2, pontos[i * 2] + 3);
+				algBres(g, pontos[i * 2], pontos[(i * 2) + 1] , pontos[(i * 2) + 2] , pontos[(i * 2) + 3] );
+				distPontos = (pontos[(i * 2) + 1] - pontos[(i * 2) + 3]);
+				distPontos = Math.abs(distPontos);
+				somaVx = somaVx + distPontos;
 			}
 
 		}
+		
+		vx = new Vx[somaVx];
+		for (int i = 0; i < somaVx; i++)
+			vx[i] = new Vx();
 
 //        lados[0] = new Lados(1,49,50,50,200);
 //        lados[1] = new Lados(2,50,200,250,-0.2666);
 //        lados[2] = new Lados(3,200,210,210,-1);
 //        lados[3] = new Lados(4,49,200,50,0.9375);
 
-		// algBres(g,px1 80,py1 0,px2 0,py2 10);
-		// algBres(g,px2 0,py2 10,px3 40,py3 30); //i=1
-		// algBres(g,px3 40,py3 30,px4 20,py4 50);//i=2
-		// algBres(g,px4 20,py4 50,px1 80,py1 0);//i=3
-		// py1 = menorY;
-		// i=0 ymin = 0 | ymax = 10 | xymin = xmin - menor y | xymin = px1 - py1 | 80 =
-		// 80 - 0 = 80
-		// if(py2 < py1 ){
-		// menorY = py2;
-		// }
-		// i=1 xymin = px2 - menorY(ainda é py1) = 0 - 0 = 0;
-		// i=2 xymin = px3 - menorY(ainda é py1) = 40 - 0 = 40;
-
 		int dx = 0;
 		int dy = 0;
 		double um_sob_m;
-		int xymin;
-		int menorY = pontos[1]; // py1
+//		int menorY = pontos[1]; // py1
+		int aux;
 
 		for (int i = 0; i < numlados; i++) {
 
-			dx = (pontos[i * 2] + 2) - (pontos[i * 2]);
-			dy = (pontos[i * 2] + 3) - (pontos[i * 2] + 1);
+			dx = pontos[(i * 2) + 2]  - pontos[i * 2];
+			dy = pontos[(i * 2) + 3] - pontos[(i * 2) + 1] ;
+			
+			dx = Math.abs(dx);
+			dy = Math.abs(dy);
+			
 			um_sob_m = dx / dy;
-
-			xymin = pontos[i * 2] - menorY;
-
-			if (xymin < 0) {
-				xymin = xymin * -1;
-			}
-
-			if (pontos[i * 2] + 1 < menorY) {
-				menorY = pontos[i * 2] + 1;
-			}
+			
+			if(pontos[(i * 2) + 3] < pontos[(i * 2) + 1]) { // se py2 for menor que py1, entao px1 = px2 e px2 = px1
+				aux = pontos[i * 2]; //aux = px1
+				pontos[i * 2] = pontos[(i * 2) + 2]; //px1 = px2
+				pontos[(i * 2) + 2] = aux; //px2 = px1
+			} //garante que o px1 sempre será o valor de XYmin 
+			
+//			if (pontos[(i * 2) + 1]  < menorY) {
+//				menorY = pontos[(i * 2) + 1] ;
+//			}
 
 //                   if(i == numlados -1) {	
 //                		lados[i] = new Lados(i + 1, pontos[i*2], pontos[i*2] + 1, pontos[0], pontos[1]);
 //                	}else {
 //                		lados[i] = new Lados(i + 1, pontos[i*2] + 2, pontos[i*2] + 3, xymin, um_sob_m);
 //        			}
+			
+			
+			if (i == numlados - 1) {
+				lados[i] = new Lados(i + 1, pontos[(i * 2) + 1] , pontos[1] , pontos[i * 2], um_sob_m);
+				
+				
+			}else {
+				lados[i] = new Lados(i + 1, pontos[(i * 2) + 1] , pontos[(i * 2) + 3] , pontos[i * 2], um_sob_m);
 
-			lados[i] = new Lados(i + 1, pontos[i * 2] + 1, pontos[i * 2] + 3, xymin, um_sob_m);
+			}
 
+			
 		}
 
 		g.setColor(Color.blue);
-		preAGeo(g);
+		preAGeo(g, somaVx);
 
 	}
 
@@ -160,7 +174,7 @@ public class Desenha extends JComponent {
 		g.drawLine(x, y, x, y);
 	}
 
-	private void preAGeo(Graphics g) {
+	private void preAGeo(Graphics g, int somaVx) {
 		int x;
 		for (int i = 0; i < 4; i++) {
 			for (int yvar = lados[i].ymin; yvar <= lados[i].ymax; yvar++) {
@@ -171,13 +185,13 @@ public class Desenha extends JComponent {
 					vx[yvar].x2 = x;
 			}
 		}
-		ordernaVx();
-		imprimeVx(g);
+		ordernaVx(somaVx);
+		imprimeVx(g, somaVx);
 
 	}
 
-	private void ordernaVx() {
-		for (int y = 0; y < 312; y++) {
+	private void ordernaVx(int somaVx) {
+		for (int y = 0; y < somaVx; y++) {
 			int aux = vx[y].x1;
 			if (vx[y].x1 > vx[y].x2) {
 				vx[y].x1 = vx[y].x2;
@@ -187,8 +201,8 @@ public class Desenha extends JComponent {
 
 	}
 
-	private void imprimeVx(Graphics g) {
-		for (int y = 0; y < 312; y++) {
+	private void imprimeVx(Graphics g, int somaVx) {
+		for (int y = 0; y < somaVx; y++) {
 			if (vx[y].x1 != -1) {
 				algBres(g, vx[y].x1, y, vx[y].x2, y);
 			}
